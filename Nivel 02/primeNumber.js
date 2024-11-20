749e46596c75bf00f138de9e02a09be779fe9c08
kaychenderson <146909378+kaychenderson@users.noreply.github.com>
2024-11-20 18:18:23 -0300

    Challenge 2.5

---
+++ Nivel 02/primeNumber.js
@@ -0,0 +1,10 @@
+function numeroPrimo(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+console.log(numeroPrimo(7)); // true
+console.log(numeroPrimo(10)); // false
